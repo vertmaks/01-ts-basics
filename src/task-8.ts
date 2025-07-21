25c0a39f0847644a7f3b386ff3b740eaf10c51b5
Maksym Veretnov <maxvertnov@gmail.com>
2025-07-21 18:38:20 +0200

    rename interface in 8 task

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-interface Posts {
+interface Post {
     id: number;
     title: string;
     body: string;
 }
 
-async function fetchPosts(): Promise<Posts[]> {
-  const response = await axios.get<Posts[]>(
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
     '<https://jsonplaceholder.typicode.com/posts>'
   );
   return response.data;
